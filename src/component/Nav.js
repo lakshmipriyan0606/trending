32a384be4e47e0c291a942ce99363f7cfbffe422
lakshmipriyan <lakshmipriyan0606@gmail.com>
2023-09-28 16:58:12 +0530

    seven commit

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -4,42 +4,55 @@ import Sidenavbar from "./Sidenavbar";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-  const [sideNavbar, setSideNavbar] = useState();
+  const [sideNavbar, setSideNavbar] = useState(false);
 
-  function sidenav() {
+  function toggleSidenav() {
     setSideNavbar(!sideNavbar);
   }
 
   return (
-    <div className="w-full">
-      <div className="flex w-full  items-center p-4 md:hidden fixed justify-between top-0 left-0   z-40 bg-[#F3F2EE]">
-        <h1 className="text-3xl font-bold md:pt-2">
-          Trend<span className=" text-[#E53637] ">ing</span>
-        </h1>
-        <FaBarsStaggered
-          className="text-2xl cursor-pointer md:hidden"
-          onClick={sidenav}
-        />
-      </div>
-      <div className="hidden md:flex md:justify-between p-3 ">
-      <h1 className="text-3xl font-bold md:pt-2">
-          Trend<span className=" text-[#E53637] ">ing</span>
-        </h1>
-        <ul className="mt-3 md:gap-9 md:mr-10 lg:gap-20  text-xl hidden md:flex ">
-              <Link to={`/`} className=" text-center hover:text-[#E53637] duration-500 transition-colors">
-                <li>Home</li>
-              </Link  >
-              {/* <Link  className=" text-center hover:text-[#E53637] duration-500 transition-colors"> <li>Shop</li></Link> */}
-              <Link to={`/about`}  className=" text-center">
-                <li className="hover:text-[#E53637] duration-500 transition-colors">About</li>
-              </Link>
-              <Link to={`/blog`}  className=" text-center hover:text-[#E53637] duration-500 transition-colors">
-                <li>Blog</li>
-              </Link>
-              <Link to={"/contact"}  className=" text-center hover:text-[#E53637] duration-500 transition-colors"><li>Contact</li></Link>
-            </ul>
+    <div>
+      <div className="fixed top-0 left-0 w-full bg-[#F3F2EE] z-50">
+        <div className="flex justify-between items-center p-4 md:hidden">
+          <h1 className="text-3xl font-bold md:pt-2">
+            Trend<span className="text-[#E53637]">ing</span>
+          </h1>
+          <FaBarsStaggered
+            className="text-2xl cursor-pointer md:hidden"
+            onClick={toggleSidenav}
+          />
         </div>
-      <Sidenavbar sideNavbar={sideNavbar} setSideNavbar = {setSideNavbar}/>
+        <div className="hidden md:flex md:justify-between p-3">
+          <h1 className="text-3xl font-bold md:pt-2">
+            Trend<span className="text-[#E53637]">ing</span>
+          </h1>
+          <ul className="mt-3 md:gap-9 md:mr-10 lg:gap-20 text-xl hidden md:flex">
+            <li>
+              <Link to="/" className="text-center hover:text-[#E53637] duration-500 transition-colors">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="text-center hover:text-[#E53637] duration-500 transition-colors">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/blog" className="text-center hover:text-[#E53637] duration-500 transition-colors">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="text-center hover:text-[#E53637] duration-500 transition-colors">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Side Navigation Bar */}
+      <Sidenavbar sideNavbar={sideNavbar} setSideNavbar={setSideNavbar} />
     </div>
   );
 };
